0779051d0c7a525bdc54743051b7c72f95a3e30b
SanyaUA <kozmey1996@mail.ua>
2018-08-07 22:47:21 +0300

    Initialize GLAD

--- Learn OpenGL - Hello Window/main.cpp
+++ Learn OpenGL - Hello Window/main.cpp
@@ -24,5 +24,11 @@ int main()
 
 	glfwMakeContextCurrent(window);
 
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD." << std::endl;
+		return -1;
+	}
+
 	return 0;
 }

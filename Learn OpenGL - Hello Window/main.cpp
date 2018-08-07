c2cd5a747aee2892421fc7b0a5ccbe08067df5b9
SanyaUA <kozmey1996@mail.ua>
2018-08-07 22:39:40 +0300

    Instantiate GLFW

--- Learn OpenGL - Hello Window/main.cpp
+++ Learn OpenGL - Hello Window/main.cpp
@@ -3,5 +3,16 @@
 
 int main()
 {
+	// instantiate glfw
+	glfwInit();
+	
+	// set major and minor version
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	
+	// set core profile for the OpenGL
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
 	return 0;
 }
